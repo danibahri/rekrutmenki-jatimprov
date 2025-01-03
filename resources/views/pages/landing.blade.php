d29e143798a093b3d639c390743218ce1caeb186
Ahmad Ramadani Bahri <164604168+danibahri@users.noreply.github.com>
2025-01-03 13:58:56 +0700

    Update landing.blade.php

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -3,51 +3,89 @@
 @section('title', 'Informasi Rekrutmen')
 
 @section('content')
-<div class="content max-w-screen-lg mx-auto p-4 py-6 lg:py-8">
     {{-- HALAMAN AWAL --}}
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-        <div class="info">
-            <h1 class="text-3xl font-semibold text-gray-900">Rekrutmen Komisi <span class="text-blue-700">Informasi</span>
-            </h1>
-            <p class="text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem
-                voluptates. Quisquam, quidem voluptates.</p>
-            <div class="flex items-center justify-center w-full mt-4">
-                <a href="#"
-                    class="flex justify-center items-center w-full py-2 bg-blue-700 rounded-lg text-white text-center border-2 hover:border-2 hover:border-blue-800 hover:bg-blue-600">Daftar
-                    Sekarang <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                        class="bi bi-hand-index-thumb mx-2" viewBox="0 0 16 16">
-                        <path
-                            d="M6.75 1a.75.75 0 0 1 .75.75V8a.5.5 0 0 0 1 0V5.467l.086-.004c.317-.012.637-.008.816.027.134.027.294.096.448.182.077.042.15.147.15.314V8a.5.5 0 0 0 1 0V6.435l.106-.01c.316-.024.584-.01.708.04.118.046.3.207.486.43.081.096.15.19.2.259V8.5a.5.5 0 1 0 1 0v-1h.342a1 1 0 0 1 .995 1.1l-.271 2.715a2.5 2.5 0 0 1-.317.991l-1.395 2.442a.5.5 0 0 1-.434.252H6.118a.5.5 0 0 1-.447-.276l-1.232-2.465-2.512-4.185a.517.517 0 0 1 .809-.631l2.41 2.41A.5.5 0 0 0 6 9.5V1.75A.75.75 0 0 1 6.75 1M8.5 4.466V1.75a1.75 1.75 0 1 0-3.5 0v6.543L3.443 6.736A1.517 1.517 0 0 0 1.07 8.588l2.491 4.153 1.215 2.43A1.5 1.5 0 0 0 6.118 16h6.302a1.5 1.5 0 0 0 1.302-.756l1.395-2.441a3.5 3.5 0 0 0 .444-1.389l.271-2.715a2 2 0 0 0-1.99-2.199h-.581a5 5 0 0 0-.195-.248c-.191-.229-.51-.568-.88-.716-.364-.146-.846-.132-1.158-.108l-.132.012a1.26 1.26 0 0 0-.56-.642 2.6 2.6 0 0 0-.738-.288c-.31-.062-.739-.058-1.05-.046zm2.094 2.025" />
-                    </svg></a>
+        <div class="relative flex flex-col md:flex-row gap-4">
+            <div class="absolute flex justify-center items-center w-full h-screen bg-gradient-to-t from-black to-transparent">
+                <div class="max-w-screen-lg p-4 w-full md:w-1/2 z-10 ">
+                    <h1 class="text-3xl font-semibold text-white mb-3">Rekrutmen Komisi <span
+                            class="text-blue-700">Informasi</span>
+                    </h1>
+                    <p class="text-white">Panitia Seleksi Rekrutmen Calon Anggota Komisi Informasi Pusat Periode 2021-2025
+                        mengundang warga negara Republik Indonesia yang telah memenuhi syarat untuk mendaftarkan diri melalui
+                        rekrutmen terbuka dalam rangka rekrutmen Calon Anggota Komisi Informasi Pusat Periode 2021-2025
+                        berdasarkan Undang-Undang Nomor 14 Tahun 2008 tentang Keterbukaan Informasi Publik.</p>
+                    <div class="flex gap-2">
+                        <div class="flex items-center justify-center w-1/2 mt-4">
+                            <a href="#"
+                                class="flex justify-center items-center w-full py-2 bg-blue-700 rounded-lg text-white text-center border-2 border-blue-700 hover:border-2 hover:border-blue-800 hover:bg-blue-600">Daftar
+                                Sekarang <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                    fill="currentColor" class="bi bi-hand-index-thumb mx-2" viewBox="0 0 16 16">
+                                    <path
+                                        d="M6.75 1a.75.75 0 0 1 .75.75V8a.5.5 0 0 0 1 0V5.467l.086-.004c.317-.012.637-.008.816.027.134.027.294.096.448.182.077.042.15.147.15.314V8a.5.5 0 0 0 1 0V6.435l.106-.01c.316-.024.584-.01.708.04.118.046.3.207.486.43.081.096.15.19.2.259V8.5a.5.5 0 1 0 1 0v-1h.342a1 1 0 0 1 .995 1.1l-.271 2.715a2.5 2.5 0 0 1-.317.991l-1.395 2.442a.5.5 0 0 1-.434.252H6.118a.5.5 0 0 1-.447-.276l-1.232-2.465-2.512-4.185a.517.517 0 0 1 .809-.631l2.41 2.41A.5.5 0 0 0 6 9.5V1.75A.75.75 0 0 1 6.75 1M8.5 4.466V1.75a1.75 1.75 0 1 0-3.5 0v6.543L3.443 6.736A1.517 1.517 0 0 0 1.07 8.588l2.491 4.153 1.215 2.43A1.5 1.5 0 0 0 6.118 16h6.302a1.5 1.5 0 0 0 1.302-.756l1.395-2.441a3.5 3.5 0 0 0 .444-1.389l.271-2.715a2 2 0 0 0-1.99-2.199h-.581a5 5 0 0 0-.195-.248c-.191-.229-.51-.568-.88-.716-.364-.146-.846-.132-1.158-.108l-.132.012a1.26 1.26 0 0 0-.56-.642 2.6 2.6 0 0 0-.738-.288c-.31-.062-.739-.058-1.05-.046zm2.094 2.025" />
+                                </svg></a>
+                        </div>
+                        <div class="flex items-center justify-center w-1/2 mt-4">
+                            <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" type="button"
+                                class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white py-3 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                                id="menu-button" aria-expanded="true" aria-haspopup="true">
+                                Pengumuman
+                                <svg class="-mr-1 size-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor"
+                                    aria-hidden="true" data-slot="icon">
+                                    <path fill-rule="evenodd"
+                                        d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                            </button>
+                            <div id="dropdown" class="hidden z-10 bg-white divide-y divide-gray-100 rounded-lg w-44 md:w-72">
+                                <ul class="py-2 text-sm text-gray-700" aria-labelledby="dropdownDefaultButton">
+                                    <li>
+                                        <a href="#" class="block px-4 py-2 hover:bg-gray-100">Dashboard</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="block px-4 py-2 hover:bg-gray-100">Settings</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="block px-4 py-2 hover:bg-gray-100">Earnings</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="block px-4 py-2 hover:bg-gray-100">Sign
+                                            out</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="grid grid-cols-2 md:grid-cols-3 gap-3 mt-4">
+                        <div class="card bg-gray-100 p-4 rounded-lg shadow-md">
+                            <h2 class="text-lg font-semibold text-gray-900 text-center">Status</h2>
+                            <hr>
+                            <p class="flex justify-center items-center text-gray-500 mt-3"><span
+                                    class="bg-red-100 text-red-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded">Di
+                                    tutup</span>
+                            </p>
+                        </div>
+                        <div class="card bg-gray-100 p-4 rounded-lg shadow-md">
+                            <h2 class="text-lg font-semibold text-gray-900 text-center">Pendaftaran</h2>
+                            <hr>
+                            <p class="text-gray-500">Terakhir : 21 September 2021</p>
+                        </div>
+                        <div class="card bg-gray-100 p-4 rounded-lg shadow-md">
+                            <h2 class="text-lg font-semibold text-gray-900 text-center">Pendaftar</h2>
+                            <hr>
+                            <p class="flex justify-center items-center text-gray-500 mt-3"><span
+                                    class="bg-green-100 text-green-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded">1.800</span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="grid grid-cols-2 md:grid-cols-3 gap-3 mt-4">
-                <div class="card bg-white p-4 rounded-lg shadow-md">
-                    <h2 class="text-lg font-semibold text-gray-900 text-center">Status</h2>
-                    <hr>
-                    <p class="flex justify-center items-center text-gray-500 mt-3"><span
-                            class="bg-red-100 text-red-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded">Di
-                            tutup</span>
-                    </p>
-                </div>
-                <div class="card bg-white p-4 rounded-lg shadow-md">
-                    <h2 class="text-lg font-semibold text-gray-900 text-center">Pendaftaran</h2>
-                    <hr>
-                    <p class="text-gray-500">Terakhir : 21 September 2021</p>
-                </div>
-                <div class="card bg-white p-4 rounded-lg shadow-md">
-                    <h2 class="text-lg font-semibold text-gray-900 text-center">Pendaftar</h2>
-                    <hr>
-                    <p class="flex justify-center items-center text-gray-500 mt-3"><span
-                            class="bg-green-100 text-green-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded">1.800</span>
-                    </p>
-                </div>
+            <div class="w-full">
+                <img src="{{ url('/image/work.jpg') }}" alt="logo" class="shadow-md w-full h-screen object-cover">
+                {{-- <div class="absolute inset-x-0 bottom-0 h-3/4 bg-black"></div> --}}
             </div>
         </div>
-        <div class="img">
-            <img src="{{ url('/image/work.jpg') }}" alt="logo" class="rounded-lg shadow-md">
-        </div>
-    </div>
 
+<div class="content max-w-screen-lg mx-auto p-4 py-6 lg:py-8">
     {{-- TABS NAVIGATION --}}
     <div class="text-sm font-medium text-center text-gray-500 border-b border-gray-200 mt-8">
         <ul class="flex flex-wrap -mb-px">
