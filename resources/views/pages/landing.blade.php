3e75e245847dc03ba192c0a8ce66cbabd4514122
danibahri <danibahri45@gmail.com>
2025-01-20 10:13:08 +0700

    update image

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -103,8 +103,7 @@
             </div>
         </div>
         <div class="w-full">
-            <img src="{{ url('/image/work.jpg') }}" alt="logo" class="shadow-md w-full h-screen object-cover">
-            {{-- <div class="absolute inset-x-0 bottom-0 h-3/4 bg-black"></div> --}}
+            <img src="{{ asset('/image/pemda.jpg') }}" alt="logo" class="shadow-md w-full h-screen object-cover">
         </div>
     </div>
 
